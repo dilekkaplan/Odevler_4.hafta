87d842a782330ba80b4194a9d407d5652af890c5
dilekkaplan <72886943+dilekkaplan@users.noreply.github.com>
2020-11-09 23:04:38 +0300

    Update soru2.2.py

--- soru2.2.py
+++ soru2.2.py
@@ -1,7 +1,7 @@
 "Bu program, içerisinde str, int, float değerleri bulunan bir demette\
 yalnızca string değerlerini bastırır."
 
-demet=(2,3,4,3.14,"bir tek",123,"DİLEĞİM",5,"var","mutlu ol", "yeter")
+demet=(2,3,4,3.14,"bir tek",123,"DİLEĞİM",5,"var","mutlu ol", "yeter","https://open.spotify.com/track/2FBoxlXWATApoFs6489Apn?si=_6T_kkvXRFuMxhyRmpqczg")
 
 for i in range(len(demet)):
     if type(demet[i])==str:
