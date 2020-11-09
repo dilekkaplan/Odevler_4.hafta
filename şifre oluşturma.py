81fb88ddd3514faeff554cefb6d86998237d2528
dilekkaplan <72886943+dilekkaplan@users.noreply.github.com>
2020-11-09 15:23:15 +0300

    Update şifre oluşturma.py

--- şifre oluşturma.py
+++ şifre oluşturma.py
@@ -6,10 +6,16 @@ while sifre.isnumeric()!=True:
     print ("HATA! Lutfen rakamlardan olusan bir şifre giriniz!\n")
     sifre=str(input("Lutfen dort haneli bir sifre giriniz: "))
 
-while len(sifre)!=4:
-    print ("HATA!", len(sifre) ," haneli bir sifre girdiniz!\n")
-    sifre=str(input("Lutfen dort haneli bir sifre giriniz: "))
+while len(sifre)!=4 or sifre.isnumeric()!=True:
+
+    if len(sifre)!=4:
+        print ("HATA!", len(sifre) ," haneli bir sifre girdiniz!\n")
+        sifre=str(input("Lutfen dort haneli bir sifre giriniz: "))
+
+    elif sifre.isnumeric()!=True:
+        print ("HATA! Lutfen rakamlardan olusan bir şifre giriniz!\n")
+        sifre=str(input("Lutfen dort haneli bir sifre giriniz: "))
+        
     
 else:
     print("\nSifre olusturuldu.\nSifreniz:", sifre)
-    
